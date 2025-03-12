a4fdee3ed56c12768b376dad3dc2004bccf45efe
Hass <hassmroueh@gmail.com>
2025-03-12 16:21:59 -0400

    added db colums

--- server/models.py
+++ server/models.py
@@ -3,43 +3,51 @@ from config import db
 import flask_login
 
 
-class User():
-    id =  db.Column()
-    name = db.Column()
-    username = db.Column()
-    _password_hash = db.Column()
-    
+class User(db, SerializerMixin):
+    __tablename__ = 'users'
+
+    id =  db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    username = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+
     applications= db.relationship()
     jobs = db.relationship()
 
 
-class Job():
-    id = db.Column ()
-    company_id = db.Column() 
-    title = db.Column()
-    description = db.Column()
-    salary = db.Column()
+class Company(db, SerializerMixin):
+    __tablename__ = 'companies'
+
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String)
+    logo = db.Column(db.String)
+    location = db.Column(db.String)
+    description = db.Column(db.String)
+
+    jobs = db.relationship()
+
+class Job(db, SerializerMixin):
+    __tablename__ = 'jobs'
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    salary = db.Column(db.Integer)
+    company_id = db.Column(db.Integer,db.ForeignKey("companies.id")) 
 
     applications = db.relationship()
     users = db.relationship()
 
-    
-class Company():
-    id = db.Column()
-    name = db.Column()
-    logo = db.Column()
-    location = db.Column()
-    description = db.Column()
 
-    jobs = db.relationship()
+class Application(db, SerializerMixin):
+    __tablename__ = 'applications'
 
-class Application():
-    id = db.Column()
-    appliction_date = db.Column()
-    status = db.Column()
-    job_id = db.Column()
-    user_id = db.Column()
+    id = db.Column(db.Integer)
+    appliction_date = db.Column(db.String)
+    status = db.Column(db.String)
+    job_id = db.Column(db.Integer, db.ForeignKey('jobs.id'))
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
 
     user = db.relationship()
     job = db.relationship()
-    
+
