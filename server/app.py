ba9b976462b38601c240131e18a16c08d02e7844
Hass <hassmroueh@gmail.com>
2025-03-17 00:18:08 -0400

    Login/Logout resources added

--- server/app.py
+++ server/app.py
@@ -1,10 +1,54 @@
 from config import api,app,db
 from models import *
+from flask import request
+from flask_restful import Resource
+from flask_login import login_user
+
+class Login(Resource):
+    def post(self):
+        username = request.get_json(['username'])
+        password = request.get_json(['password'])
+
+        user = User.query.filter_by(username=username).first()
+        if user and user.authenticate(password):
+            login_user(user,remember=True)
+            return user.to_dict(rules = '-_password_hash' ), 200
+
+class SignUp(Resource):
+    def post(self):
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+        user = User(username=username)
+        user._password_hash = password
+        db.session.add()
+        db.session.commit()
+        return user.to_dict(rules = '-_password_hash' ),201
+
+class Logout(Resource):
+    def delete(self):
+        pass
+
+class Companies(Resource):
+    def get(self):
+        pass
+
+class Jobs(Resource):
+    def get(self):
+        pass
 
 
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
+
+
+
+
+
+
+
+api.add_resource(Login, '/login')
+
+
+
+
 
 
 
