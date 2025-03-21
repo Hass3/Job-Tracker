835593c503e5be956fba9c92eeb9b2190ff2ecbf
Hass <hassmroueh@gmail.com>
2025-03-21 01:53:49 -0400

    edit form layout

--- client/src/components/CompanyDetails.js
+++ client/src/components/CompanyDetails.js
@@ -2,9 +2,11 @@ import { useParams } from "react-router-dom"
 import { useState,useEffect } from "react"
 import NavBar from "./NavBar"
 import CompanyJobs from "./CompanyJobs"
+import CompanyEditForm from "./CompanyEditForm"
 
 function CompanyDetails(){
    const [company, setCompany] = useState(null)
+   const [formOn, setFormOn] = useState(false)
    const parms = useParams()
    const companyId = parms.id
 
@@ -15,6 +17,8 @@ function CompanyDetails(){
         setCompany(c)
        })
    }, [])
+
+    const formBtn = ()=> setFormOn(on=>!on)
 
 if (!company){return <h1>Loading!</h1>}
 const {name, logo, description, head_quarters, jobs} = company
@@ -27,6 +31,8 @@ return(
 <img src={logo}/>
 <h2>{description}</h2>
 <h4>Head Quarters: {head_quarters}</h4>
+<button onClick={formBtn}>{formOn ? 'back': 'edit'}</button>
+{formOn ? <CompanyEditForm comapny={company}/>: null}
 {jobs.map((job)=>
 <CompanyJobs 
 key={job.id}
---
+++ client/src/components/CompanyEditForm.js
@@ -0,0 +1,64 @@
+import { useFormik } from "formik"
+import * as yup from 'yup'
+
+
+
+function CompanyEditForm({comapny}){
+    const fromSchema = yup.object().shape({
+        name: yup.string().required("Name Required"),
+        logo: yup.string().required('Logo url Required'), 
+        description: yup.string().required('Description Required'),
+        headQuarters:yup.string().required('Head Quarters Required')
+      })
+
+    const formik = useFormik(({
+        initialValues:{
+            name: `${comapny.name}`,
+            logo:`${comapny.logo}`,
+            description:`${comapny.description}`,
+            headQuarters: `${comapny.headQuarters}`
+        },
+        validationSchema:fromSchema,
+        onSubmit:(values)=>{
+            
+        }
+    }))
+
+    return(
+        <>
+        <form>
+            <input
+            name="name"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            />
+             <input
+            name="logo"
+            value={formik.values.logo}
+            onChange={formik.handleChange}
+            />
+             <input
+            name="description"
+            value={formik.values.description}
+            onChange={formik.handleChange}
+            />
+             <input
+            name="headQuarters"
+            value={formik.values.headQuarters}
+            onChange={formik.handleChange}
+            />
+
+            <button type="submit">Complete</button>
+
+
+        </form>
+    
+        </>
+    )
+
+
+
+
+}
+
+export default CompanyEditForm
