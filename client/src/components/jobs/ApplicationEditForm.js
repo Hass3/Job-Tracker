91642ba5e932b5d59bf2dc3334222ed74ae64105
Hass <hassmroueh@gmail.com>
2025-04-09 18:00:29 -0400

    Application edit Form Formatted

---
+++ client/src/components/jobs/ApplicationEditForm.js
@@ -0,0 +1,24 @@
+import { useFormik } from "formik"
+import * as yup from 'yup'
+
+
+
+function ApplicationEditForm(){
+
+return(
+<>
+<form>
+  <input />
+  <input/>
+  <button>Complete</button>
+</form>
+
+</>
+
+)
+
+}
+
+
+
+export default ApplicationEditForm
